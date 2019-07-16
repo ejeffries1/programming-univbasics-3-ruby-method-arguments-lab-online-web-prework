9a76d4dc128b82e45181c9518ab22d939530b2df
Evan Jeffries <ejeffries14@gmail.com>
2019-07-16 17:48:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language(name,language)
 end
 
 def introduction_with_language_optional(name,language)
-  if language == "Ruby"
+  if language = "Ruby"
     puts "Hi, my name is #{name} and I am learning to program in Ruby."
   else
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
